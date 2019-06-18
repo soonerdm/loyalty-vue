edf5a69381c2faf30e4ed05c0741a531c767fe07
Josh Willson <jwillson@buyforlessok.com>
2019-06-18 13:43:32 -0500

    Moved notify scripts to app.js

--- public/js/app.js
+++ public/js/app.js
@@ -50653,6 +50653,43 @@ var app = new Vue({
   el: '#app'
 });
 
+Notify = function Notify(text, callback, close_callback, style) {
+  var time = '10000';
+  var $container = $('#notifications');
+  var icon = '<i class="fa fa-info-circle "></i>';
+  if (typeof style === 'undefined') style = 'warning';
+  var html = $('<div class="alert alert-' + style + '  hide">' + icon + " " + text + '</div>');
+  $('<a>', {
+    text: '×',
+    "class": 'button close',
+    style: 'padding-left: 10px;',
+    href: '#',
+    click: function click(e) {
+      e.preventDefault();
+      close_callback && close_callback();
+      remove_notice();
+    }
+  }).prependTo(html);
+  $container.prepend(html);
+  html.removeClass('hide').hide().fadeIn('slow');
+
+  function remove_notice() {
+    html.stop().fadeOut('slow').remove();
+  }
+
+  var timer = setInterval(remove_notice, time);
+  $(html).hover(function () {
+    clearInterval(timer);
+  }, function () {
+    timer = setInterval(remove_notice, time);
+  });
+  html.on('click', function () {
+    clearInterval(timer);
+    callback && callback();
+    remove_notice();
+  });
+};
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
