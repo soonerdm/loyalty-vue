daf5a9ae1186495926e5ea598cf26ee5fd01c6c8
David Meinke <davidmeinke@Davids-MacBook-Pro.local>
2019-06-21 09:20:11 -0500

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src=/img/"@php echo explode('.', $_SERVER['HTTP_HOST'])[1]; @endphp.png" height="50">
+                    <img src='/img/@php echo explode('.', $_SERVER['HTTP_HOST'])[1]; @endphp.png' height="50">
                    <!-- <img src="/img/logo.png" height="50" /> -->
                 </a>
                 {{--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">--}}
